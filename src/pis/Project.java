9b7acc2937a9df9989dae6e5f16511ab1f4234d4
Marek Milkovič <milkovic.marek@gmail.com>
2016-04-20 19:18:11 +0200

    Initial commit.

---
+++ src/pis/Project.java
@@ -0,0 +1,32 @@
+package pis;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "project")
+public class Project {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
