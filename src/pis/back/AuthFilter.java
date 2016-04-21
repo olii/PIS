2ee76c02cdfb1ee339bcbb3f4f9ee5c19b336bf7
Marek Milkovič <milkovic.marek@gmail.com>
2016-04-22 00:37:23 +0200

    Added basic AuthFilter.

---
+++ src/pis/back/AuthFilter.java
@@ -0,0 +1,43 @@
+package pis.back;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthFilter implements Filter {
+	@SuppressWarnings("unused")
+	private FilterConfig filterConfig;
+	
+	@Override
+	public void destroy() {	
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpReq = (HttpServletRequest)req;
+		HttpSession session = httpReq.getSession();
+		AuthBean authBean = (AuthBean)session.getAttribute("authBean");
+ 
+		System.out.println(httpReq.getRequestURI());
+		boolean noRedirect = httpReq.getRequestURI().startsWith("/pis/projects/login.xhtml");
+		if (noRedirect || (authBean != null && authBean.isLoggedIn())) {
+			chain.doFilter(req, resp);
+			return;
+		}
+
+		((HttpServletResponse)resp).sendRedirect(httpReq.getContextPath() + "/projects/login.xhtml");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+}
