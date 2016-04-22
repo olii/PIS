4d84e9318cab90c4e20f10c8c11b3ccf84a2ee57
Marek Milkovič <milkovic.marek@gmail.com>
2016-04-22 20:29:58 +0200

    Added SQL file with default accounts for initial DB setup.

---
+++ sql/accounts.sql
@@ -0,0 +1,4 @@
+DELETE FROM person;
+INSERT INTO person (login, password, name, surname, mail, acc_type) VALUES
+("teacher", "teacher", "Default", "Teacher", "teacher@uni.edu", "TEACHER"),
+("student", "student", "Default", "Student", "student@uni.edu", "STUDENT");
