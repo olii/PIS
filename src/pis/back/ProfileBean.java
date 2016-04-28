eca53fc9b047333ea827f21098278951f5facc5a
olii <oliver.nemcek@gmail.com>
2016-04-28 20:04:56 +0200

    check validity of email in profile update method

--- src/pis/back/ProfileBean.java
+++ src/pis/back/ProfileBean.java
@@ -2,9 +2,12 @@ package pis.back;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 import pis.data.Person;
 import pis.service.PersonManager;
@@ -17,8 +20,18 @@ public class ProfileBean {
 	private Person account;
 	private String newPassword;
 	
+	private String email;
+	
 	public Person getAccount() {
 		return account;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public void setAccount(Person account) {
@@ -38,12 +51,35 @@ public class ProfileBean {
 		AuthBean authBean = (AuthBean)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("authBean");
 		account = authBean.getAccount();
 		newPassword = "";
+		email = account.getMail();
 	}
-
+	
+	public boolean isValidEmailAddress(String email) {
+	   boolean result = true;
+	   try {
+	      InternetAddress emailAddr = new InternetAddress(email);
+	      emailAddr.validate();
+	   } catch (AddressException ex) {
+	      result = false;
+	   }
+	   return result;
+	}
+	
 	public String saveProfile() {
 		if (!newPassword.equals("")) {
 			account.setPassword(newPassword);
 		}
+		
+		if (isValidEmailAddress(email)){
+			account.setMail(email);
+			System.out.println("OK");
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid email address", ""));
+			return "";
+		}
+		
+		
 		
 		personMgr.save(account);
 		return "profile-saved";
